16d968e405880e31e9a081bb81897a9b5de0f5fe
Alejandro Calle-Prado <alex_calle@hotmail.com>
2024-03-29 10:28:59 +0100

    chore: set up and check unittest for VSCode

--- restapi/tests.py
+++ restapi/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+# from django.test import TestCase
+from unittest import TestCase
+
 
 # Create your tests here.
+def sum_of_two_integers(a, b):
+    return a + b
+
+
+class TestSum(TestCase):
+    def test_sum(self):
+        self.assertEqual(sum_of_two_integers(1, 2), 3)
+        self.assertEqual(sum_of_two_integers(2, 2), 4)
