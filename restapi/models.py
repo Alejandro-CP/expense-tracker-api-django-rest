0b2bf0f10482a704ff96cc80b66677e819566e4d
Alejandro Calle-Prado <alex_calle@hotmail.com>
2024-03-29 11:57:16 +0100

    chore: Expense model and test. Update workflow for django tests

--- restapi/models.py
+++ restapi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    amount = models.FloatField()
+    merchant = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    category = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
